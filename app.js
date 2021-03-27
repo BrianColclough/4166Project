99d12d585e8b2afabec09299770f5fba2285bcfc
Brian <brianmcolclough@gmail.com>
2021-03-27 18:17:09 -0400

    Milestone 2 complete and start of milestone 3

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+var connections = require('./assets/connectionDB');
+var connectionsModel = connections.connect();
+
+app.set('view engine','ejs');
+//static resources
+app.use('/assets', express.static('assets'));
+
+
+var index = require('./controller/index')
+app.use('/', index);
+
+var library = require('./controller/controller');
+app.use('/connection', library);
+
+app.listen(8080,function(){
+   console.log('app started');
+   console.log('listening on port 8080');
+});
